a59ac8c8abf3f23581a7e7e904bf980307643014
KashanAdnan-Coder <computertechub0@gmail.com>
2024-08-28 17:56:26 +0500

    dom querySelector

---
+++ Advance/Class 2/script.js
@@ -0,0 +1,8 @@
+// Document Object Model
+// let h1 = document.getElementsByTagName("h1")
+// console.log(h1);
+
+
+// let main_container = document.getElementsByClassName("main-container")[0].style.backgroundColor = "blue"
+
+// let main_container = document.querySelector(".main-container").style.backgroundColor = "blue";
