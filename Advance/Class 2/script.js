d97fd827181e9d94e7d7dfbf3936dfe6da678158
KashanAdnan-Coder <computertechub0@gmail.com>
2024-08-28 18:09:23 +0500

    dom creation setting attribute appending child add text

--- Advance/Class 2/script.js
+++ Advance/Class 2/script.js
@@ -6,3 +6,16 @@
 // let main_container = document.getElementsByClassName("main-container")[0].style.backgroundColor = "blue"
 
 // let main_container = document.querySelector(".main-container").style.backgroundColor = "blue";
+
+// let anchor = document.createElement("a")
+// anchor.setAttribute("href", "https://github.com/KashanAdnan-Coder/online-batch-01-kashan-coding/tree/main")
+// anchor.setAttribute("class", "link")
+
+// anchor.innerText = "Go to the Github Link Of Online Batch 01 Kashan Coding Teaching Shabea"
+// console.log(anchor);
+
+// let main_container = document.querySelector(".main-container")
+// main_container.appendChild(anchor)
+// console.log(main_container);
+
+// document.write("Kashan")
