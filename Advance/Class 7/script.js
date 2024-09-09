ea570160ec75c627fedc92502e042ac4129d5e7c
KashanAdnan-Coder <computertechub0@gmail.com>
2024-09-09 17:40:27 +0500

    JavaScript Ends

---
+++ Advance/Class 7/script.js
@@ -0,0 +1,11 @@
+// es5 function
+// function getName(){
+
+// }
+
+// es6 or beyond
+const getName = () => {
+    
+}
+
+// https://excalidraw.com/#json=y-keE_it17i1V1auZwhVB,98s8oEmkOK11xw3T5ET-vw
